b4ad4ce0fcea038ab4a9ca54d32ea6eda38a742f
mikanmarusan <chiakifujimon@yahoo.co.jp>
2020-06-06 15:24:23 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mikanmarusan/go-cli-sample/lib"
+)
+
+func main() {
+	fmt.Println(greeting.Greeting())
+}
---
+++ main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mikanmarusan/go-cli-sample/lib"
+)
+
+func ExampleGreeting() {
+	fmt.Println(greeting.Greeting())
+	// Output:
+	// Hello, World
+}
